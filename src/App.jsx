2d9235c71c6388208dbaa1221912154afba7b22f
Celal Karakoç <c.karakoc@live.nl>
2025-11-15 12:15:47 +0100

    Quick small commit;

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Rating from './components/Rating';
 const App = () => {
     return (
         <div>
-            <Rating/>
+            <Rating heading="Rate your Experience" color="gold"/>
         </div>
     );
 };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,23 +1,38 @@
-export const Rating = () => {
+import {useState} from "react";
+
+export const Rating = ({heading = 'Rating', color = 'gold'}) => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
     const stars = Array.from({length: 5}, (_, i) => i + 1);
+    const feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent'];
 
     /*const clicked = (index) => console.log(`Star ${index} clicked!`);
     const hovered = (status) => console.log(`Current hover status: ${status}`);*/
 
     return (
         <div className='rating-container' /*style={styles.container}*/>
-            <h2 /*style={styles.heading}*/> Rate your Experience </h2>
+            <h2 /*style={styles.heading}*/> {heading} </h2>
             <div className="stars">
                 {stars.map((star, index) => (
                     <span
                         /*onClick={() => clicked(index)}
                         onMouseEnter={() => hovered('enter')}
                         onMouseLeave={() => hovered('leave')}*/
+                        onClick={() => setRating(star)}
+                        onMouseEnter={() => setHover(star)}
+                        onMouseLeave={() => setHover(0)}
                         key={star}
-                        className='star'>{'\u2605'}
+                        className='star'
+                        style={{
+                            color: star <= (hover || rating) ? color : 'lightgray'
+                        }}
+                        /*className={`star ${star <= (hover || rating) ? 'active' : ''}`}*/
+                    >{'\u2605'}
                     </span>
                 ))}
             </div>
+            {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
         </div>
     )
 };
